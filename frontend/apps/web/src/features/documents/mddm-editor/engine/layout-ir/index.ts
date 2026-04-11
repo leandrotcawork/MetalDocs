50ca6f8bc047989b41d05706ee0425233ff6cb6a
leandrotcawork <leandrotca.work@gmail.com>
2026-04-11 00:21:11 -0300

    feat(mddm-engine): add layout-ir barrel export

---
+++ frontend/apps/web/src/features/documents/mddm-editor/engine/layout-ir/index.ts
@@ -0,0 +1,14 @@
+export { defaultLayoutTokens, type LayoutTokens } from "./tokens";
+export {
+  defaultComponentRules,
+  type ComponentRules,
+  type SectionRule,
+  type FieldRule,
+  type FieldGroupRule,
+} from "./components";
+export {
+  COMPATIBILITY_CONTRACT,
+  isForbiddenConstruct,
+  type CompatibilityContract,
+  type ForbiddenConstruct,
+} from "./compatibility-contract";

72db4e003c28398338aa94f465939d9536f0082a
leandrotcawork <leandrotca.work@gmail.com>
2026-04-11 00:31:49 -0300

    feat(mddm-engine): add asset-resolver barrel export

---
+++ frontend/apps/web/src/features/documents/mddm-editor/engine/asset-resolver/index.ts
@@ -0,0 +1,8 @@
+export { isAllowlistedAssetUrl } from "./allowlist";
+export { RESOURCE_CEILINGS, ResourceCeilingExceededError, type ResourceCeilings } from "./ceilings";
+export {
+  AssetResolver,
+  AssetResolverError,
+  type AllowedMimeType,
+  type ResolvedAsset,
+} from "./asset-resolver";

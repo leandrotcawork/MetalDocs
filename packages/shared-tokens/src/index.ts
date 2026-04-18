9f407523cafd926d28b5d03ae87b9236dec7c84d
leandrotcawork <leandrotca.work@gmail.com>
2026-04-18 13:00:20 -0300

    feat(shared-tokens): public barrel

--- packages/shared-tokens/src/index.ts
+++ packages/shared-tokens/src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { parseDocxTokens } from './parser';
+export { diffTokensVsSchema, type SchemaDiff } from './diff';
+export { WHITELIST, BLACKLIST, classifyBlacklist, isElementAllowed } from './ooxml';
+export { IDENT_RE, RESERVED_IDENTS, MAX_SECTION_DEPTH, isValidIdent, isReservedIdent } from './grammar';
+export type { Token, TokenKind, ParseError, ParseResult } from './types';
